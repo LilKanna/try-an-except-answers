b158054c75cdc6e9c348257a464cc88f6e0e306f
omerOP1321 <azaloni123@gmail.com>
2022-05-17 17:48:38 +0300

    try and except answers 1 - 2

---
+++ try and execpt answers.py
@@ -0,0 +1,24 @@
+# answer for ex1
+
+#def ex1(num):
+     #try:
+        #return 6 / num
+     #except:
+         #print(f'dont divide by {num}')
+#print(ex1("asd"))
+
+#answer for ex2
+
+def ex2(index, my_tuple=(1,2,3,4,5)):
+    try:
+        return my_tuple[index]
+    except:
+        print(f'couldnt find value in position {index} ')
+print(ex2(index = 8))
+
+
+#def ex3(name):
+    #return "hello" + name
+#if __name__ == '__main__':
+    # Run your functions here
+   # print("Good Luck!")
