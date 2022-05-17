9c84fa65408b6e213847e3f5062e520f445ff6a9
omerOP1321 <azaloni123@gmail.com>
2022-05-17 18:00:45 +0300

    try and except answers 1 - 3

--- try and execpt answers.py
+++ try and execpt answers.py
@@ -9,16 +9,22 @@
 
 #answer for ex2
 
-def ex2(index, my_tuple=(1,2,3,4,5)):
-    try:
-        return my_tuple[index]
-    except:
-        print(f'couldnt find value in position {index} ')
-print(ex2(index = 8))
+#def ex2(index, my_tuple=(1,2,3,4,5)):
+   #try:
+        #return my_tuple[index]
+    #except:
+        #print(f'couldnt find value in position {index} ')
+#print(ex2(index = 8))
 
+#answer for ex3
+
+
+#if __name__ == '__main__':
+    #print("Good Luck!")
 
 #def ex3(name):
-    #return "hello" + name
-#if __name__ == '__main__':
-    # Run your functions here
-   # print("Good Luck!")
+    #try:
+        #return "hello" + name
+    #except:
+        #print("Cannot get numbers only strings")
+#print(ex3(99))
